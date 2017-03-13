197242252dbb301083b2e8478e43e08822337a15
xiaojun <xiaojun@pornhub,gov>
2017-03-12 17:11:53 -0800

    edition1.0

---
+++ build/js/index.js
@@ -0,0 +1,43 @@
+function calculate() {
+	document.getElementById("mark1").innerHTML = "";
+	document.getElementById("mark2").innerHTML = "";
+	document.getElementById("mark3").innerHTML = "";
+	document.getElementById("mark4").innerHTML = "";
+	var c1 = document.getElementById("cir1").value;
+	var c2 = document.getElementById("cir2").value;
+	var c3 = document.getElementById("cir3").value;
+	var c4 = 1;
+	var result = parseInt(document.getElementById("input").value);
+	var arr = [];
+	arr.push(parseInt(c1));
+	arr.push(parseInt(c2));
+	arr.push(parseInt(c3));
+	arr.push(parseInt(c4));
+	var brr = [];
+	brr.push(parseInt(c1));
+	brr.push(parseInt(c2));
+	brr.push(parseInt(c3));
+	brr.push(parseInt(c4));
+	arr.sort(function(a, b) {
+		return b - a;
+	})
+	console.log(arr);
+	console.log(brr);
+	var map = {};
+	var i = 0;
+	console.log(arr)
+	while (i <= 3) {
+		if (arr[i] != NaN && result >= arr[i])
+			while (Math.floor(result / arr[i]) > 0) {
+				map[i] = Math.floor(result / arr[i]);
+				result = result % arr[i];
+			}
+		i++;
+	}
+	console.log(map)
+	for (var i in map) {
+		var position = "mark" + (brr.indexOf(arr[i]) + 1);
+		//document.getElementById(position).innerHTML = arr[i];
+		document.getElementById(position).innerHTML = "<div class='mark' id='mark1'>" + map[i] + "</div>";
+	}
+}
---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var open = require('open');
+
+var app = {
+  srcPath: 'src/',
+  devPath: 'build/',
+  prdPath: 'dist/'
+};
+
+gulp.task('lib', function() {
+  gulp.src('bower_components/**/*.js')
+    .pipe(gulp.dest(app.prdPath + 'vendor'))
+    .pipe($.connect.reload());
+});
+
+gulp.task('libcss', function() {
+  gulp.src('bower_components/**/*.css')
+    .pipe(gulp.dest(app.prdPath + 'vendor'))
+    .pipe($.connect.reload());
+});
+
+gulp.task('html', function() {
+  gulp.src(app.srcPath + '**/*.html')
+    .pipe(gulp.dest(app.prdPath))
+    .pipe($.connect.reload());
+})
+
+
+gulp.task('less', function() {
+  gulp.src(app.srcPath + 'style/**/*.less')
+    .pipe($.plumber())
+    .pipe($.less())
+    .pipe(gulp.dest(app.devPath + 'css'))
+    .pipe($.cssmin())
+    .pipe(gulp.dest(app.prdPath + 'css'))
+    .pipe($.connect.reload());
+});
+
+gulp.task('js', function() {
+  gulp.src(app.srcPath + 'script/**/*.js')
+    .pipe($.plumber()) //asdfasdfasdf
+    .pipe($.concat('index.js'))
+    .pipe(gulp.dest(app.devPath + 'js'))
+    .pipe($.uglify())
+    .pipe(gulp.dest(app.prdPath + 'js'))
+    .pipe($.connect.reload());
+});
+
+gulp.task('image', function() {
+  gulp.src(app.srcPath + 'image/**/*')
+    .pipe($.plumber())
+    .pipe(gulp.dest(app.devPath + 'image'))
+    .pipe($.imagemin())
+    .pipe(gulp.dest(app.prdPath + 'image'))
+    .pipe($.connect.reload());
+});
+
+gulp.task('build', ['image', 'js', 'less', 'lib', 'libcss', 'html']);
+
+gulp.task('clean', function() {
+  gulp.src([app.devPath, app.prdPath])
+    .pipe($.clean());
+});
+
+gulp.task('serve', ['build'], function() {
+  $.connect.server({
+    root: [app.prdPath],
+    livereload: true,
+    port: 3131
+  });
+
+  open('http://localhost:3131');
+
+  gulp.watch('bower_components/**/*', ['lib']);
+  gulp.watch(app.srcPath + '**/*.html', ['html']);
+  gulp.watch(app.srcPath + 'style/**/*.less', ['less']);
+  gulp.watch(app.srcPath + 'script/**/*.js', ['js']);
+  gulp.watch(app.srcPath + 'image/**/*', ['image']);
+});
+
+gulp.task('default', ['serve']);
